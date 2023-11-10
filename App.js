81f387fe6d32b4743a0ecd33befb89b9ba3bc15b
yuri <yjdutra@gmail.com>
2023-11-10 10:54:52 -0300

    fix celan open target app

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import OpenURLButton from './src/components/OpenUrlButton';
-import OPenTargetApp from './src/components/OpenTargetApp';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Playground for try new features</Text>
-      <OPenTargetApp/>
       <OpenURLButton url={'controltabletsmartek'}>
         'Open appTablet Tools witch react-native lib'
       </OpenURLButton>
